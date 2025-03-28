3220be5deaf544ad27f8c7ccee0bbdfccf9fcae9
Ryel-2-1 <espinili.ryeljan@gmail.com>
2025-03-28 23:46:46 +0800

    Add files via upload

---
+++ Actions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.BusinessDataLogic
+{
+    public enum Actions
+    {
+        AddNote,
+        DeleteNote,
+        View
+
+    }
+}
---
+++ NoteProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Note.BusinessDataLogic
+{
+    public class NoteProcess
+    {
+        public static List<string> notes = new List<string>();
+        public static bool UpdateNotes(Actions userAction, string input = null)
+        {
+            if (userAction == Actions.AddNote && !string.IsNullOrWhiteSpace(input))
+            {
+                notes.Add(input);
+                return true;
+            }
+
+            if (userAction == Actions.DeleteNote && notes.Count > 0 &&
+                int.TryParse(input, out int index) && index >= 1 && index <= notes.Count)
+            {
+                notes.RemoveAt(index - 1);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public static List<string> GetNotes()
+        {
+            return notes;
+        }
+        public static bool HasNotes()
+        {
+            return notes.Count > 0;
+        }//try
+    }
+}
